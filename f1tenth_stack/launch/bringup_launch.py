dec5340cfb0767fa19b3956876fa28583e9c2b57
OpenZeka <info@openzeka.com>
2023-01-08 17:41:36 +0300

    Added zed2 and lidar nodes to launch file

--- f1tenth_stack/launch/bringup_launch.py
+++ f1tenth_stack/launch/bringup_launch.py
@@ -20,122 +20,123 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.substitutions import Command
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
-from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
 import os
 
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+
+
 def generate_launch_description():
-    joy_teleop_config = os.path.join(
-        get_package_share_directory('f1tenth_stack'),
-        'config',
-        'joy_teleop.yaml'
+
+    rviz_config_dir = os.path.join(
+        get_package_share_directory("sllidar_ros2"), "rviz", "sllidar_ros2.rviz"
     )
+
     vesc_config = os.path.join(
-        get_package_share_directory('f1tenth_stack'),
-        'config',
-        'vesc.yaml'
+        get_package_share_directory("f1tenth_stack"), "config", "vesc.yaml"
     )
     sensors_config = os.path.join(
-        get_package_share_directory('f1tenth_stack'),
-        'config',
-        'sensors.yaml'
+        get_package_share_directory("f1tenth_stack"), "config", "sensors.yaml"
     )
     mux_config = os.path.join(
-        get_package_share_directory('f1tenth_stack'),
-        'config',
-        'mux.yaml'
+        get_package_share_directory("f1tenth_stack"), "config", "mux.yaml"
     )
 
-    joy_la = DeclareLaunchArgument(
-        'joy_config',
-        default_value=joy_teleop_config,
-        description='Descriptions for joy and joy_teleop configs')
     vesc_la = DeclareLaunchArgument(
-        'vesc_config',
+        "vesc_config",
         default_value=vesc_config,
-        description='Descriptions for vesc configs')
+        description="Descriptions for vesc configs",
+    )
     sensors_la = DeclareLaunchArgument(
-        'sensors_config',
+        "sensors_config",
         default_value=sensors_config,
-        description='Descriptions for sensor configs')
+        description="Descriptions for sensor configs",
+    )
     mux_la = DeclareLaunchArgument(
-        'mux_config',
+        "mux_config",
         default_value=mux_config,
-        description='Descriptions for ackermann mux configs')
-
-    ld = LaunchDescription([joy_la, vesc_la, sensors_la, mux_la])
-
-    joy_node = Node(
-        package='joy',
-        executable='joy_node',
-        name='joy',
-        parameters=[LaunchConfiguration('joy_config')]
+        description="Descriptions for ackermann mux configs",
     )
-    joy_teleop_node = Node(
-        package='joy_teleop',
-        executable='joy_teleop',
-        name='joy_teleop',
-        parameters=[LaunchConfiguration('joy_config')]
-    )
+
+    ld = LaunchDescription([vesc_la, sensors_la, mux_la])
+
     ackermann_to_vesc_node = Node(
-        package='vesc_ackermann',
-        executable='ackermann_to_vesc_node',
-        name='ackermann_to_vesc_node',
-        parameters=[LaunchConfiguration('vesc_config')]
+        package="vesc_ackermann",
+        executable="ackermann_to_vesc_node",
+        name="ackermann_to_vesc_node",
+        parameters=[LaunchConfiguration("vesc_config")],
     )
     vesc_to_odom_node = Node(
-        package='vesc_ackermann',
-        executable='vesc_to_odom_node',
-        name='vesc_to_odom_node',
-        parameters=[LaunchConfiguration('vesc_config')]
+        package="vesc_ackermann",
+        executable="vesc_to_odom_node",
+        name="vesc_to_odom_node",
+        parameters=[LaunchConfiguration("vesc_config")],
     )
     vesc_driver_node = Node(
-        package='vesc_driver',
-        executable='vesc_driver_node',
-        name='vesc_driver_node',
-        parameters=[LaunchConfiguration('vesc_config')]
+        package="vesc_driver",
+        executable="vesc_driver_node",
+        name="vesc_driver_node",
+        parameters=[LaunchConfiguration("vesc_config")],
     )
-    throttle_interpolator_node = Node(
-        package='f1tenth_stack',
-        executable='throttle_interpolator',
-        name='throttle_interpolator',
-        parameters=[LaunchConfiguration('vesc_config')]
-    )
-    urg_node = Node(
-        package='urg_node',
-        executable='urg_node_driver',
-        name='urg_node',
-        parameters=[LaunchConfiguration('sensors_config')]
-    )
+
     ackermann_mux_node = Node(
-        package='ackermann_mux',
-        executable='ackermann_mux',
-        name='ackermann_mux',
-        parameters=[LaunchConfiguration('mux_config')],
-        remappings=[('ackermann_cmd_out', 'ackermann_drive')]
+        package="ackermann_mux",
+        executable="ackermann_mux",
+        name="ackermann_mux",
+        parameters=[LaunchConfiguration("mux_config")],
+        remappings=[("ackermann_cmd_out", "ackermann_drive")],
     )
     static_tf_node = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='static_baselink_to_laser',
-        arguments=['0.27', '0.0', '0.11', '0.0', '0.0', '0.0', 'base_link', 'laser']
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_baselink_to_laser",
+        arguments=["0.27", "0.0", "0.11", "0.0", "0.0", "0.0", "base_link", "laser"],
+    )
+
+    joy_node = Node(
+        package="joy",
+        executable="joy_node",
+        name="joy",
+    )
+
+    # Camera model (force value)
+    camera_model = "zed2"
+
+    # ZED Wrapper node
+    zed_wrapper_launch = IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+            [
+                get_package_share_directory("zed_wrapper"),
+                "/launch/include/zed_camera.launch.py",
+            ]
+        ),
+        launch_arguments={"camera_model": camera_model}.items(),
+    )
+
+    rplidar_launch = IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+            [
+                get_package_share_directory("sllidar_ros2"),
+                "/launch/sllidar_s1_launch.py",
+            ]
+        ),
     )
 
     # finalize
-    ld.add_action(joy_node)
-    ld.add_action(joy_teleop_node)
     ld.add_action(ackermann_to_vesc_node)
     ld.add_action(vesc_to_odom_node)
     ld.add_action(vesc_driver_node)
     # ld.add_action(throttle_interpolator_node)
-    ld.add_action(urg_node)
+    ld.add_action(joy_node)
     ld.add_action(ackermann_mux_node)
     ld.add_action(static_tf_node)
+    ld.add_action(zed_wrapper_launch)
+    ld.add_action(rplidar_launch)
 
     return ld
